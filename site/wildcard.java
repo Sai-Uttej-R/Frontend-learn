df5564ab0e23daa556ab23cc502761cf591fb84d
Sai Uttej <saiuttej.r@gmail.com>
2023-07-18 11:32:15 +0530

    Module3 solution

---
+++ site/wildcard.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class wildcard
+{
+    public static <T elements Number> void printArray(T[] elements)
+    {
+        for (T elements:elements)
+        {
+            System.our.println();
+        }
+        public static void wildcard(Arraylist? extends Number>n)
+            for (Number s:n)
+        {
+            System.out.println(s+"");
+        }
+        public static void main (String args[])
+        {
+            Integer[] int Array={1,2,3,4,5};
+            ArrayList <Integer> l=new ArrayList<>();
+            PrintArray(int Array);
+            l.add(54);
+        }
+    }
+}
